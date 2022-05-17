efd8fb4a207c1f9b837dafb347cd66428b781240
adnanh <adhodzic@student.unipu.hr>
2022-05-17 21:22:24 +0200

    react

---
+++ client/src/components/Login/Login.js
@@ -0,0 +1,11 @@
+
+function Login() {
+  return (
+    <div className="Login">
+        <input type="text" name="username" id="username-input" />
+        <input type="text" name="password" />
+    </div>
+  );
+}
+
+export default App;

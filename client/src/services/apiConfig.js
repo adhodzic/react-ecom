3e767c9d16586ab34978fff8ad83f182784508c6
adnanh <adhodzic@student.unipu.hr>
2022-09-04 12:38:39 +0200

    conf

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -8,6 +8,6 @@ api.interceptors.response.use((response) => response, (error) => {
     if (error.response.status === 401) {
         window.location = '/login';
     }
-    })
+})
 
 export default api;

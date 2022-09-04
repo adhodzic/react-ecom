a4dc6fc114aefa1d7f9426e77b97e9c8ff7e1d0e
adnanh <adhodzic@student.unipu.hr>
2022-09-04 13:30:43 +0200

    conf

--- service/app.js
+++ service/app.js
@@ -4,10 +4,20 @@ const cors = require('cors');
 const database = require("./services/database");
 require('dotenv').config()
 
+const https = require('https');
+const fs = require('fs');
+
 const jwt = require('jsonwebtoken')
 const userRouter = require('./routes/user.routes.js')
 const commonRouter = require('./routes/common.routes.js')
 const itemRouter = require('./routes/item.routes.js')
+
+var key = fs.readFileSync(__dirname + '../../https/selfsigned.key');
+var cert = fs.readFileSync(__dirname + '../../https/selfsigned.crt');
+var options = {
+  key: key,
+  cert: cert
+};
 
 const app = express();
 
@@ -16,6 +26,8 @@ database.dbConnect()
 app.use(bodyParser.json())
 app.use('/api',[userRouter, commonRouter,itemRouter])
 
+var server = https.createServer(options, app);
+
 const port = process.env.PORT || 5000
 
-app.listen(port, ()=>{console.log("Server is open on port 5000")});
+server.listen(port, ()=>{console.log("Server is open on port 5000")});
